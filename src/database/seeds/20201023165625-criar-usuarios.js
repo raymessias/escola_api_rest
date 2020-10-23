1a261202763202a945e853fb5d241032caf0703f
Raynon Messias <raymessias@gmail.com>
2020-10-23 14:19:04 -0300

    Create seed usuarios

---
+++ src/database/seeds/20201023165625-criar-usuarios.js
@@ -0,0 +1,39 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert('users',
+      [
+        {
+          nome: 'Marcos',
+          email: 'marcos@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Marcos2',
+          email: 'marcos2@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Marcos3',
+          email: 'marcos3@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Marcos4',
+          email: 'marcos4@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ], {});
+  },
+
+  down: () => { },
+};
