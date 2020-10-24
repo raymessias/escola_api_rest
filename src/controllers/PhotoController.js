794e3ca5430a4f8f510a40017dc49b8bc9ec6abb
Raynon Messias <raymessias@gmail.com>
2020-10-24 20:16:43 -0300

    Update: upload files

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,7 +1,17 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('filefield');
+
 class PhotoController {
   async store(req, res) {
-    res.status(200).json({
-      OK: true,
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+      res.json(req.file);
     });
   }
 }
