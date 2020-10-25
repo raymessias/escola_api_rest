e046330c8bd3d319d95f1c1ebef91f1ac45cf658
Raynon Messias <raymessias@gmail.com>
2020-10-24 21:13:54 -0300

    Create: filename on database

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,17 +1,23 @@
 import multer from 'multer';
 import multerConfig from '../config/multerConfig';
+import Photo from '../models/Photo';
 
 const upload = multer(multerConfig).single('filefield');
 
 class PhotoController {
-  async store(req, res) {
-    return upload(req, res, (err) => {
+  store(req, res) {
+    return upload(req, res, async (err) => {
       if (err) {
         return res.status(400).json({
           errors: [err.code],
         });
       }
-      res.json(req.file);
+
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+
+      const photo = await Photo.create({ originalname, filename, aluno_id });
+      return res.json(photo);
     });
   }
 }
