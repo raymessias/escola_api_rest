f389edcf6780138af0143dc6d0616c895bc90ced
Raynon Messias <raymessias@gmail.com>
2020-10-25 22:53:58 -0300

    Update: Remove folder build

--- dist/controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});class HomeController {
-  index(req, res) {
-    res.status(200).json({
-      OK: true,
-    });
-  }
-}
-
-exports. default = new HomeController();
